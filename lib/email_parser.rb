5504dff0534f24ab1609ad9bd32dd556b19beeee
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-04 03:25:58 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,11 +5,11 @@
 class EmailParser
   attr_accessor :emails
   def initialize(emails)
-    @emails=emails 
+    @emails=emails
   end
   def parse
-    emails.split(/[\s,]/)
-    emails.uniq
+    @emails.split(/[\s,]/)
+    return @emails.uniq
   end
 
 end
