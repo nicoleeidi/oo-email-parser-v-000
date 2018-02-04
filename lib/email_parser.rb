f15ad3af7873a21025c1c728ee7b91ae13807a7c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-04 03:31:08 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   def parse
     array= @emails.split(/[\s,]/)
     array.map do |email|
-      if email == "" 
+      if email == ""
         array.delete(email)
       end
     end
